d9fab141f532994b31236f686856a64656d518bf
Rafał Tęcza <rafaltecza@MacBook-Pro-Rafa.local>
2022-09-06 01:51:56 +0200

    Finalization

---
+++ animations.js
@@ -0,0 +1,3 @@
+import img_1 from './images/img_0.jpg';
+
+export default {}
--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.use(cors());
 const jsonParser = bodyParser.json()
 
 app.get('/', (req, res) => {
-    res.send('Motion API')
+    res.send('Motion Bridge - Factory API')
 });
 
 app.post('/render', jsonParser, async (req, res) => {
     console.log(req.body);
-    const { lottieAnimation, animationConfiguration } = req.body;
-    const { fileName, extension } = animationConfiguration;
+    const {lottieAnimation, animationConfiguration} = req.body;
+    const {fileName, extension} = animationConfiguration;
 
     return await renderLottie({
         animationData: lottieAnimation,
@@ -26,7 +26,13 @@ app.post('/render', jsonParser, async (req, res) => {
 })
 
 app.listen(port, async () => {
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`Motion Bridge API Listening to ${port}!`)
 });
 
-
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(8080, '0.0.0.0', function() {
+    console.log('Running CORS Anywhere on ' + '0.0.0.0' + ':' + 8080);
+});
